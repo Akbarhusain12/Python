603e8715a6a59c45e2c93192d92db4ebc1b5fe66
Kadiwala Akbar husain <akbarhusainkadiwala32@gmail.com>
2024-12-14 19:19:54 +0530

    update and add new file

---
+++ Test/area.py
@@ -0,0 +1,12 @@
+def area_of_circle(r):
+    pi = 3.14
+    result = pi * r * r
+    return result
+
+def area_of_triangle(b, h):
+    result = 0.5 * b * h
+    return result
+
+def area_of_rectangle(l, w):
+    result = l * w
+    return result
